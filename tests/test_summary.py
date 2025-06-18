65a4750367d7084b27990e351a5c8968a594b941
fxwiegand <fxwiegand@wgdnet.de>
2025-06-18 12:33:50 +0200

    feat: Add very basic summarize skeleton code

---
+++ tests/test_summary.py
@@ -0,0 +1,21 @@
+from varpubs.pubmed_db import PubmedArticle
+from varpubs.summarize import PubmedSummarizer, HFSettings
+import os
+
+
+def test_summarization():
+    article = PubmedArticle(
+        pmid=12345678,
+        title="Genetic variants in BRCA1 and breast cancer risk",
+        abstract="BRCA1 mutations significantly increase the risk of breast and ovarian cancers. In this study, we evaluate the frequency and pathogenicity of BRCA1 variants in a European cohort consisting of 1000 participants. Out of these, 777 developed breast cancer and 123 developed ovarian cancer within 2 years.",
+        authors="Smith J, Doe A",
+        journal="Genetics Today",
+        pub_date="2023-09-15",
+        doi="10.1234/genetictoday.2023.456",
+    )
+
+    settings = HFSettings(token=os.environ["HF_TOKEN"])
+
+    summarizer = PubmedSummarizer(settings)
+    summary = summarizer.summarize(article)
+    print(summary)
