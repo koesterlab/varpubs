79103d6a69008518de1a91b8e0a5c183b19ce61c
Felix Wiegand <fxwiegand@wgdnet.de>
2025-07-09 16:11:42 +0200

    refactor: Format with ruff

--- src/varpubs/test_extractor.py
+++ src/varpubs/test_extractor.py
@@ -1,5 +1,6 @@
 from varpubs.hgvs_extractor import extract_hgvsp_from_vcf
 
+
 def main():
     vcf_path = "/Users/ilmaytas/Desktop/annotated.vcf"
     hgvsp_list = extract_hgvsp_from_vcf(vcf_path)
@@ -10,6 +11,7 @@ def main():
     with open("output_pubmed_terms.txt", "w") as f:
         for item in hgvsp_list:
             f.write(item + "\n")
+
 
 if __name__ == "__main__":
     main()
