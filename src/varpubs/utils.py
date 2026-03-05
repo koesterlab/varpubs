4956b674164e039a09e04d893a3ba978bb3592b1
fxwiegand <fxwiegand@wgdnet.de>
2026-03-05 07:31:13 +0100

    Bump cyvcf2 and enable header removal
    Uncomment vcf.remove_header in extend_vep_header now that the
    upstream cyvcf2 fix for header removal has landed (requires >=0.32.0)

--- src/varpubs/utils.py
+++ src/varpubs/utils.py
@@ -7,7 +7,7 @@ def extend_vep_header(vcf: VCF, new_fields: List[str], csq_id="CSQ"):
     parts = description.split("'")
     to_add = " | ".join(new_fields)
     new_description = f"{parts[0]}{parts[1]} | {to_add}{parts[2]}"
-    # vcf.remove_header(csq_id) This doesnt work yet. Waiting for https://github.com/brentp/cyvcf2/pull/323
+    vcf.remove_header(csq_id)
     vcf.add_info_to_header(
         {
             "ID": csq_id,
