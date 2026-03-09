10c9fe0397d40fa9c8f23f98ba08d665300bba8c
Felix Mölder <felix.moelder@uk-essen.de>
2026-03-09 09:53:15 +0100

    fix header extension

--- src/varpubs/utils.py
+++ src/varpubs/utils.py
@@ -7,7 +7,7 @@ def extend_vep_header(vcf: VCF, new_fields: List[str], csq_id="CSQ"):
     description = vcf.get_header_type(csq_id)["Description"]
     parts = description.split("'")
     to_add = " | ".join(new_fields)
-    new_description = f"{parts[0]}{parts[1]} | {to_add}{parts[2]}"
+    new_description = f"{parts[0][1:]}'{parts[1]} | {to_add}'"
     vcf.remove_header(csq_id)
     vcf.add_info_to_header(
         {
