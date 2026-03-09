cc2a395e3608236dc0fc3db7b33bd2e3ec16098d
Felix Mölder <felix.moelder@uk-essen.de>
2026-03-09 12:24:41 +0100

    Support vep ANN field

--- src/varpubs/utils.py
+++ src/varpubs/utils.py
@@ -5,9 +5,8 @@ from cyvcf2 import VCF
 
 def extend_vep_header(vcf: VCF, new_fields: List[str], csq_id="CSQ"):
     description = vcf.get_header_type(csq_id)["Description"]
-    parts = description.split("'")
     to_add = " | ".join(new_fields)
-    new_description = f"{parts[0][1:]}'{parts[1]} | {to_add}'"
+    new_description = f"{description[1:-1]} | {to_add}"
     vcf.remove_header(csq_id)
     vcf.add_info_to_header(
         {
